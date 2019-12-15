039f57ff729c64b835bc7cb3c54b2851a87eb275
pleshakoff <pleshakoff@gmail.com>
2019-12-15 12:26:26 +0300

    Функционал создания контролной суммы

---
+++ src/main/java/com/parcom/security_client/Checksum.java
@@ -0,0 +1,67 @@
+package com.parcom.security_client;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.DefaultClaims;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.session.SessionAuthenticationException;
+
+import javax.validation.constraints.NotNull;
+import java.util.Collection;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+
+@Slf4j
+public class Checksum extends TokenUtils {
+
+	private static final String KEY =  "bF$HTVG4t\"BB\"JLY/W>)uX!";
+	private static final String CHECKSUM = "checksum";
+	private static final String CHECKSUM_ERROR = "You can't call this method directly. Use Slassrom service for registration";
+	private static final String IDENTIFIER = "IDENTIFIER";
+
+	public static String createChecksum(@NotNull Long id) {
+        if (id == null) {
+			 throw new  RuntimeException("Id can't be null");
+		}
+
+		Date now = new Date();
+		Claims claims = Jwts.claims().setSubject(CHECKSUM);
+		claims.put(IDENTIFIER, id);
+
+		return Jwts.builder()
+				.setClaims(claims)
+				.setIssuedAt(new Date())
+				.setExpiration( new Date(now.getTime() + TimeUnit.MINUTES.toMillis(5)))
+				.signWith(SignatureAlgorithm.HS512, KEY)
+				.compact();
+	}
+
+
+	static void validateCheckSum(String checksum,Long id)	{
+
+		DefaultClaims claims;
+		try {
+			claims = (DefaultClaims) Jwts.parser().setSigningKey(KEY).parse(checksum).getBody();
+		} catch (Exception ex) {
+			log.error("Check sum error",ex);
+			throw new RuntimeException(CHECKSUM_ERROR);
+		}
+		Date expiredDate = claims.getExpiration();
+		if (expiredDate == null) {
+			throw new RuntimeException(CHECKSUM_ERROR);		}
+		if (!expiredDate.after(new Date())) {
+			throw new RuntimeException(CHECKSUM_ERROR);
+		}
+		Long checksumId = claims.get(IDENTIFIER, Long.class);
+		if (checksumId == null||!checksumId.equals(id)) {
+			throw new RuntimeException(CHECKSUM_ERROR);
+		}
+	}
+
+}
---
+++ src/test/java/com/parcom/security_client/ChecksumTest.java
@@ -0,0 +1,45 @@
+package com.parcom.security_client;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ChecksumTest {
+
+
+    private static final long ID = 12345L;
+
+    @Test
+    public void createChecksum() {
+        Checksum.createChecksum(ID);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void createChecksumNull() {
+        Checksum.createChecksum(null);
+    }
+
+
+
+    @Test
+    public void validateCheckSum() {
+        Checksum.validateCheckSum(Checksum.createChecksum(ID),ID);
+
+    }
+
+
+    @Test(expected = RuntimeException.class)
+    public void validateCheckSumWrong() {
+        Checksum.validateCheckSum(Checksum.createChecksum(ID),123L);
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void validateCheckSumNull() {
+        Checksum.validateCheckSum(Checksum.createChecksum(ID),null);
+
+    }
+
+
+}
