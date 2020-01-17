acb371f3afb0041fa3556a64014be9c7d4ef01b1
pleshakoff <pleshakoff@gmail.com>
2020-01-18 00:42:04 +0300

    Перенес все в одну конфигурацию

--- src/main/java/com/parcom/security_client/ParcomWebSecurityConfigurerAdapter.java
+++ src/main/java/com/parcom/security_client/ParcomWebSecurityConfigurerAdapter.java
@@ -1,51 +1,52 @@
 package com.parcom.security_client;
 
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
-import org.springframework.boot.actuate.health.HealthEndpoint;
-import org.springframework.boot.actuate.info.InfoEndpoint;
-import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusScrapeEndpoint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
-@Import(SecurityClientConfiguration.class)
 @ConditionalOnMissingBean(ParcomWebSecurityConfigurerAdapter.class)
 public class ParcomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
-    protected final  UnauthorizedEntryPoint unauthorizedEntryPoint;
+    @Autowired
+    private MessageSource messageSource;
 
-    protected final AuthenticationTokenProcessingFilter authenticationTokenProcessingFilter;
-
-    public ParcomWebSecurityConfigurerAdapter(UnauthorizedEntryPoint unauthorizedEntryPoint, AuthenticationTokenProcessingFilter authenticationTokenProcessingFilter) {
-        this.unauthorizedEntryPoint = unauthorizedEntryPoint;
-        this.authenticationTokenProcessingFilter = authenticationTokenProcessingFilter;
+    @Bean
+    AuthenticationTokenProcessingFilter authenticationTokenProcessingFilter()
+    {
+        return   new AuthenticationTokenProcessingFilter(messageSource);
     }
 
+    @Bean
+    UnauthorizedEntryPoint unauthorizedEntryPoint() {
+        return new UnauthorizedEntryPoint(messageSource);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.
                 authorizeRequests((requests) -> {
-                    requests.requestMatchers(EndpointRequest.to(HealthEndpoint.class, InfoEndpoint.class, PrometheusScrapeEndpoint.class)).permitAll();
                     requests.antMatchers(
                             "/webjars/springfox-swagger-ui/**",
                             "/swagger-ui.html/**",
                             "/swagger-resources/**",
-                            "/v2/api-docs").
+                            "/v2/api-docs",
+                            "/actuator/**").
                             permitAll();
                     requests.anyRequest().authenticated();
                 });
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.csrf().disable();
-        http.exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint);
-        http.addFilterBefore(authenticationTokenProcessingFilter, UsernamePasswordAuthenticationFilter.class);
+        http.exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint());
+        http.addFilterBefore(authenticationTokenProcessingFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
 
